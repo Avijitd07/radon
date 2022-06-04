943b3ebbf5ae4be1c6371c90e03aa29b1975e600
Avijit <happyavijit1998@gmail.com>
2022-06-04 22:21:26 +0530

    mongoDB practice

--- src/index.js
+++ src/index.js
@@ -4,17 +4,24 @@ const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://cluster0.6evf0.mongodb.net/?retryWrites=true&w=majority",
+{
+    dBname: 'My_database',
+    user: 'Avijit07',
+    pass: 'Avijit@1998',
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 app.use('/', route);
+
 
 
 app.listen(process.env.PORT || 3000, function () {
