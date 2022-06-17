af550ad52098817dd764b236587c3ed1fc8dd1ea
Avijit07 <happyavijit1998@gmail.com>
2022-06-17 16:02:29 +0530

    tr_catch/statuscode

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,33 +2,47 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (req, res) {
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  res.send({ msg: savedData });
+  try{
+    let data = req.body;
+    if(Object.keys(data).length!=0){
+      let savedData = await userModel.create(data);
+      res.status(201).send({ msg: savedData });
+    }else{
+      res.status(400).send({msg: "Bad Request" });
+    }
+  }catch(error){
+    console.log(error.message);
+    res.status(500).send({msg: error.message});
+  }
 };
 
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
   let password = req.body.password;
 
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  // console.log(user)
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
+  try{
 
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FunctionUp",
-    },
-    "functionup-radon"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    // console.log(user)
+    if (!user)
+      return res.status(400).send({
+        status: false,
+        msg: "BAD REQUEST: username or the password is not corerct"
+      });
+
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "thorium",
+        organisation: "FunctionUp",
+      },
+      "functionup-radon"
+    );
+    res.setHeader("x-auth-token", token);        //["x-auth-token"]= token
+    res.status(201).send({ status: true, token: token });
+  }catch(error){
+    res.status(500).send({msg:"Server Error"})
+  }
 };
 
 const getUserData = async function (req, res) {
@@ -73,7 +87,7 @@ const postMessage = async function (req, res) {
 
     
     
-    let userId= req.params.userId
+    let uerId= req.params.userId
     let user = await userModel.findById(userId)
     if(!user) return res.send({status: false, msg: 'No such user exists'})
     
